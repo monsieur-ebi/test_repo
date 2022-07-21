2a14c46232693e6f28894b1a4fab396256c4d1a8
Monsieur_ebi <ebizimordisi123@gmail.com>
2022-07-21 15:54:43 +0100

    Updates

---
+++ class.js
@@ -0,0 +1,25 @@
+class Human {
+  constructor() {
+    this.gender = "Male";
+  }
+
+  printGender() {
+    console.log(this.gender);
+  }
+}
+
+class Person extends Human {
+  constructor() {
+    super();
+    this.name = "zimor";
+    this.secondName = "zimor.near";
+  }
+
+  printName() {
+    console.log(this.name);
+  }
+}
+
+const person = new Person();
+person.printName();
+person.printGender();
